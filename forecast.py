38017d97b3de1f6ac474113c705c85fc88e26879
Jasmine Trevino <1jasminejtrevino1@gmail.com>
2026-04-14 16:05:41 -0500

    change forecast message in main

--- forecast.py
+++ forecast.py
@@ -5,7 +5,7 @@ conditions = ["Sunny", "Cloudy", "Rain", "Thunderstorm", "Windy"]
 temp = random.randint(70, 100)
 condition = random.choice(conditions)
 
-print("Today's forecast:")
+print("Branch version forecast:")
 print(f"Condition: {condition}")
 print(f"Temperature: {temp}°F")
 
