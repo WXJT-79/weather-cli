19c84e9bb80c44b95ef833b9178051e9ea04c075
Jasmine Trevino <1jasminejtrevino1@gmail.com>
2026-04-14 16:04:06 -0500

    change forcast message

--- forecast.py
+++ forecast.py
@@ -5,9 +5,10 @@ conditions = ["Sunny", "Cloudy", "Rain", "Thunderstorm", "Windy"]
 temp = random.randint(70, 100)
 condition = random.choice(conditions)
 
-print("Today's forecast:")
+print("Branch version forecast:")
 print(f"Condition: {condition}")
 print(f"Temperature: {temp}°F")
 
 humidity = random.randint(40, 100)
 print(f"Humidity: {humidity}%")
+
